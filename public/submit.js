db690aff2b639965882cc80219d87b1570e333a5
Allan Artuso <allan.artuso@gmail.com>
2018-08-12 01:47:16 +0200

    Layout update

--- public/submit.js
+++ public/submit.js
@@ -1,33 +1,33 @@
 var form = document.getElementById("formProduct");
 
-form.onsubmit = function (e) {
-      e.preventDefault();
-      var result = document.getElementById("result");
-      var url = '/products';
+form.onsubmit = function(e) {
+  e.preventDefault();
+  var result = document.getElementById("result");
+  var url = "/products";
 
-      var digits = checkBox(form.elements['digits']);
-      var numbers = checkBox(form.elements['numbers']);
-      var names = checkBox(form.elements['names']);
+  var digits = checkBox(form.elements["digits"]);
+  var numbers = checkBox(form.elements["numbers"]);
+  var names = checkBox(form.elements["names"]);
 
-      var sendData = { digits: digits, numbers: numbers, names: names };
+  var sendData = { digits: digits, numbers: numbers, names: names };
 
-      //Using JQuery
-      $.ajax({
-            url: url,
-            type: "POST",
-            data: JSON.stringify(sendData),
-            contentType: "application/json",
-            dataType: 'json',
-            success: function (success) {
-                  result.innerHTML = JSON.stringify(success);
-                  form.reset();
-            },
-            error: function (error) {
-                  result.innerHTML = JSON.stringify(error);
-            }
-      });
+  //Using JQuery
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: JSON.stringify(sendData),
+    contentType: "application/json",
+    dataType: "json",
+    success: function(success) {
+      result.innerHTML = JSON.stringify(success, null, 2);
+      form.reset();
+    },
+    error: function(error) {
+      result.innerHTML = JSON.stringify(error, null, 2);
+    }
+  });
 
-      /*
+  /*
       //Pure JS
       var ajax = new XMLHttpRequest();
 
@@ -39,7 +39,7 @@ form.onsubmit = function (e) {
 
             if (ajax.readyState == 4 && ajax.status == 200) {
                   var success = JSON.parse(ajax.responseText);
-                  result.innerHTML = JSON.stringify(success);
+                  result.innerHTML = JSON.stringify(success, null, 2);
                   form.reset();
             } else {
                   var error = ajax.responseText + ajax.status + ' (' + ajax.statusText + ')';
@@ -47,15 +47,15 @@ form.onsubmit = function (e) {
             }
       }
       */
-}
+};
 
 function checkBox(myArray) {
-      var checked = [];
+  var checked = [];
 
-      for (var i = 0, len = myArray.length; i < len; i++) {
-            if (myArray[i].checked) {
-                  checked.push(myArray[i].value);
-            }
-      }
-      return checked;
+  for (var i = 0, len = myArray.length; i < len; i++) {
+    if (myArray[i].checked) {
+      checked.push(myArray[i].value);
+    }
+  }
+  return checked;
 }
